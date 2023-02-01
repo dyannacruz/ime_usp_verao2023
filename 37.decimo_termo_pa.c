a3655216246e18f7d63eebc5b7e8327efba058da
Dyanna Cruz <90336853+dyannacruz@users.noreply.github.com>
2023-02-01 17:56:28 -0300

    Add files via upload

---
+++ 37.decimo_termo_pa.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+        int termo1, razao, pa;
+        scanf("%d %d", &termo1, &razao);
+        
+        pa = termo1 + 9 * razao;
+        
+        printf("%d", pa);
+}
