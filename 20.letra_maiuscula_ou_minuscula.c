7e2c1002c851704dcba144fe9bbcb00c30b271c4
Dyanna Cruz <90336853+dyannacruz@users.noreply.github.com>
2023-02-01 17:21:53 -0300

    Add files via upload

---
+++ 20.letra_maiuscula_ou_minuscula.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char main()
+{
+    char caractare;
+    scanf("%c", &caractare);
+    
+    if (caractare == 'a'|| caractare == 'b'|| caractare == 'c'||caractare == 'd'||caractare == 'e'||caractare == 'f'||caractare == 'g'||caractare == 'h'||caractare == 'i'||caractare == 'j'||caractare == 'k'||caractare == 'l'||caractare == 'm'||caractare == 'n'||caractare == 'o'||caractare == 'p'||caractare == 'q'||caractare == 'r'||caractare == 's'||caractare == 't'||caractare == 'u'||caractare == 'v'||caractare == 'x'||caractare == 'z'||caractare == 'w'||caractare == 'y') 
+    printf("MINUSCULA");
+    else
+    printf("MAIUSCULA");
+}
